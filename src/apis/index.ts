0adc0906d1683b6849f1cffb535bc81124cc5d79
Simone Marcato <simone@hevelop.com>
2024-06-04 14:33:13 +0200

    Generated client for service: product-configurator with version: 1.1.0 in language: typescript

---
+++ src/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ConfigurationApi';
+export * from './ConfiguratorApi';
+export * from './DependencyApi';
+export * from './MatrixApi';
+export * from './OptionApi';
+export * from './ProductConfiguratorApi';
+export * from './PropertyApi';
+export * from './StepApi';
